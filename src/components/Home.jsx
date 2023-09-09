7921ff7f3af9ae5e600d9da2ee02e3d13fbd1309
Rajan Prajapati <rajanprajapati743@gmail.com>
2023-09-09 14:42:17 +0530

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <>
+            <center>
+                <h1 className='text-black-600'>hello new project</h1>
+            </center>
+
+        </>
+    )
+}
+
+export default Home;
